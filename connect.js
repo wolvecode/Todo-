85f9667db526d7a81375a74b042eaee52c8f96a6
wolvecode <wolvecode@gmail.com>
2020-02-03 23:37:36 +0100

    changes made in database connection

--- connect.js
+++ connect.js
@@ -4,4 +4,3 @@ mongoose.connect(process.env.MONGO_DB,
 { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log('connected to database'))
     .catch(console.error)
-
